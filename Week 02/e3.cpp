98c705b591cd0ddd8a0ec6e149571a198563d462
muhammadzaeemarshad <120157283+muhammadzaeemarshad@users.noreply.github.com>
2022-12-27 08:03:04 +0500

    Week 02

---
+++ Week 02/e3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "       A     " <<endl;
+cout << "       B     " <<endl;
+cout << "       C     " <<endl;
+cout << "       U     " <<endl;
+cout << "       E     " <<endl;
+cout << "       D     " <<endl;
+}
